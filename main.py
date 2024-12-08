e0d21fd846103707bbe7291692b8ab6ce2ef9210
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-12-08 21:10:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,9 +39,8 @@ def generate_image_with_kivotos(prompt: str) -> BytesIO:
     retries = 5
     for _ in range(retries):
         random_proxy = get_random_proxy_from_file()
-        proxies = {"http": f"http://{random_proxy}", "https": f"http://{random_proxy}"}
+        proxies = {"http": f"http://{random_proxy}"}
         os.environ["http_proxy"] = proxies["http"]
-        os.environ["https_proxy"] = proxies["https"]
         print(f"Using proxy: {proxies}")
         break
 
