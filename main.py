4a6dca1067624ec1556caed6497b224c5fefc2aa
TerserahGw <120995106+TerserahGw@users.noreply.github.com>
2024-12-08 21:52:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,47 +4,27 @@ from gradio_client import Client
 from apscheduler.schedulers.background import BackgroundScheduler
 from io import BytesIO
 import os
-import random
-import signal
 import sys
+import subprocess
 
 app = FastAPI()
 
 def restart_server():
     print("Restarting server...")
-    os.kill(os.getpid(), signal.SIGINT)
+    python = sys.executable
+    uvicorn_cmd = ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080", "--reload"]
+    subprocess.Popen(uvicorn_cmd)
+    sys.exit(0)
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(restart_server, 'interval', minutes=3)
 scheduler.start()
-
-def get_random_proxy_from_file(file_path="all.txt"):
-    try:
-        with open(file_path, "r") as file:
-            proxies = file.readlines()
-            proxies = [proxy.strip() for proxy in proxies if proxy.strip()]
-            if not proxies:
-                raise HTTPException(status_code=500, detail="No proxies found in the file")
-            selected_proxy = random.choice(proxies)
-            print(f"Selected proxy: {selected_proxy}")
-            return selected_proxy
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error reading proxy file: {str(e)}")
 
 @app.get("/")
 def read_root():
     return {"status": "Server is running. Try /kivotos?text="}
 
 def generate_image_with_kivotos(prompt: str) -> BytesIO:
-    retries = 5
-    for _ in range(retries):
-        random_proxy = get_random_proxy_from_file()
-        proxies = {"http": f"http://{random_proxy}", "https": f"http://{random_proxy}"}
-        os.environ["http_proxy"] = proxies["http"]
-        print(f"Using proxy: {proxies}")
-        break
-
-    print(f"Generating image with prompt: {prompt}")
     client = Client("Linaqruf/kivotos-xl-2.0")
     result = client.predict(
         prompt=prompt,
@@ -62,8 +42,6 @@ def generate_image_with_kivotos(prompt: str) -> BytesIO:
         add_quality_tags=True,
         api_name="/run"
     )
-
-    print(f"Result Kivotos: {result}")
 
     image_path = result[0][0].get('image')
 
@@ -84,8 +62,8 @@ def kivotos_endpoint(text: str = Query(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 2020))
+    port = int(os.getenv("PORT", 8080))
     try:
-        uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+        uvicorn.run(app, host="0.0.0.0", port=port)
     finally:
         scheduler.shutdown()
